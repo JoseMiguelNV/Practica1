fdcaba148fd6bd712f7ed27dd0c78604a6b7b8a7
hertote <71886137+hertote@users.noreply.github.com>
2020-12-01 15:36:11 +0100

    Commint 6
    App terminada.

--- PracticaApp/PracticaApp/Program.cs
+++ PracticaApp/PracticaApp/Program.cs
@@ -5,10 +5,10 @@ namespace PracticaApp
 {
     class Program
     {
-        public static void Main(string[] args) // Main crea una instancia de la clase Programa, y llama a la función Run.
+        public static void Main(string[] args) 
         {
-            Programa p = new Programa();       // Es creada la clase programa.
-            p.Run();                           // Desde aqui le llama a la función Run.
+            Programa p = new Programa();       
+            p.Run();                           
         } 
     }
 }
